7e4dc6a442d3f6cb9b0bc1015b08648a752e1ac0
isaac-pm <isaacpalma21@gmail.com>
2026-03-17 10:32:42 +0100

    Parser added

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
-from minizinc import Instance, Model, Solver
+from psplib import parse
 
-# Load n-Queens model from file
-model = Model("./model.mzn")
-# Find the MiniZinc solver configuration for Gecode
-gecode = Solver.lookup("gecode")
-# Create an Instance of the n-Queens model for Gecode
-instance = Instance(gecode, model)
-# Assign 4 to n
-instance["n"] = 4
-result = instance.solve()
-# Output the array q
-print(result["q"])
+instance = parse("data/j30.sm/j301_1.sm", instance_format="psplib")
+print(instance.num_resources)
+print(instance.num_activities)
+print(instance.activities)
+
+# from minizinc import Instance, Model, Solver
+
+# # Load n-Queens model from file
+# model = Model("./model.mzn")
+# # Find the MiniZinc solver configuration for Gecode
+# gecode = Solver.lookup("gecode")
+# # Create an Instance of the n-Queens model for Gecode
+# instance = Instance(gecode, model)
+# # Assign 4 to n
+# instance["n"] = 4
+# result = instance.solve()
+# # Output the array q
+# print(result["q"])
